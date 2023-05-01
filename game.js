32c6c9d6889fe05d4fd04fc1d995bdfb931477d7
Nat <nhaba003@fiu.edu>
2023-05-01 00:23:24 -0400

    setting files

---
+++ game.js
@@ -0,0 +1,8 @@
+
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+const pacmanFrames = document.getElementById("animation");
+const ghostFrames = document.getElementById("ghosts");
+let createRect = () => {
+    
+}
